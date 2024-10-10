391a7952fe20712ed8cc9293d9355afbc4317001
Bartek <bartosz.smuga@yahoo.com>
2024-10-10 12:20:14 +0200

    simplify circle generator fn

--- deep_circle_counter/dataset/circle_dataset.py
+++ deep_circle_counter/dataset/circle_dataset.py
@@ -2,6 +2,7 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset
 
+from deep_circle_counter.dataset.generate_circles import generate_circles
 
 class CircleDataset(Dataset):
     def __init__(self, table_path: str) -> None:
@@ -13,6 +14,7 @@ class CircleDataset(Dataset):
     def __len__(self) -> int:
         raise NotImplementedError
 
-    @classmethod
-    def generate_table(cls, num_samples: int, table_path: str) -> "CircleDataset":
-        raise NotImplementedError
+    @staticmethod
+    def generate_data(num_samples: int, table_path: str) -> pd.DataFrame:
+        table = pd.DataFrame([generate_circles(200, 200, [10, 20, 25, 35])] for _ in range(num_samples))
+        pass
--- deep_circle_counter/dataset/generate_circles.py
+++ deep_circle_counter/dataset/generate_circles.py
@@ -1,35 +1,21 @@
+from dataclasses import dataclass
 import random
 
-import pandas as pd
+from sklearn.metrics.pairwise import pairwise_distances_argmin_min
 
+@dataclass
+class Circle:
+    radius: int
+    x_center: int
+    y_center: int 
 
-def _distribute(num_items: int, num_bins: int) -> list[int]:
-    distribution = [0] * num_bins
-    for _ in range(num_items):
-        distribution[random.randrange(num_bins)] += 1
-    return distribution
-
-
-def _generate_centers(radiuses: list[int], offsets: list[int]) -> list[int]:
-    centers = [offsets[0] + radiuses[0]]
-    for i in range(len(radiuses) - 1):
-        centers.append(centers[-1] + radiuses[i] + offsets[i + 1] + radiuses[i + 1])
-    return centers
-
-
-def generate_circles(
-    height: int, width: int, radiuses: list[int]
-) -> list[tuple[int, int, int]]:
-    diameter_sum = 2 * sum(radiuses)
-    num_circles = len(radiuses)
-    assert diameter_sum < min(height, width)
-
-    radiuses = random.sample(radiuses, k=len(radiuses))
-
-    offsets_x_distributed = _distribute(width - diameter_sum, num_circles + 1)[:-1]
-    offsets_y_distributed = _distribute(height - diameter_sum, num_circles + 1)[:-1]
-
-    centers_x = _generate_centers(radiuses, offsets_x_distributed)
-    centers_y = _generate_centers(radiuses, offsets_y_distributed)
-
-    return [circle for circle in zip(centers_y, centers_x, radiuses)]
+def generate_circle(size: tuple[int, int], number: int) -> list[Circle]:
+    centers = set()
+    while len(centers) == number:
+        centers.add((random.randint(1, size[0]-1), random.randint(1, size[1]-1)))
+    
+    circles = list()
+    for center in list(centers):
+        X = [[center[0], 0], [0, center[1]]] + 
+        radius = pairwise_distances_argmin_min()
+        circles.append()

7ab54cbe85579c4d122921a39fd7116e549a99d2
Bartek <bartosz.smuga@yahoo.com>
2024-10-20 05:29:12 +0200

    circle-generator-proposition

--- deep_circle_counter/dataset/generate_circles.py
+++ deep_circle_counter/dataset/generate_circles.py
@@ -1,21 +1,56 @@
-from dataclasses import dataclass
 import random
+from dataclasses import dataclass
 
-from sklearn.metrics.pairwise import pairwise_distances_argmin_min
+import numpy as np
+
 
 @dataclass
 class Circle:
-    radius: int
-    x_center: int
-    y_center: int 
+    x: int
+    y: int
+    r: int
 
-def generate_circle(size: tuple[int, int], number: int) -> list[Circle]:
-    centers = set()
-    while len(centers) == number:
-        centers.add((random.randint(1, size[0]-1), random.randint(1, size[1]-1)))
-    
-    circles = list()
-    for center in list(centers):
-        X = [[center[0], 0], [0, center[1]]] + 
-        radius = pairwise_distances_argmin_min()
-        circles.append()
+
+def generate_circles(size: tuple[int, int], points: int) -> list[Circle]:
+    x_centers = sorted(random.choices(range(size[0]), k=points))
+    y_centers = random.choices(range(size[1]), k=points)
+
+    distances = np.concat(
+        [
+            np.full((points, points), np.inf),
+            np.array([[x, y] for (x, y) in zip(x_centers, y_centers)]),
+        ],
+        axis=1,
+    )
+
+    radiuses = np.zeros(points, dtype=int)
+    for i in range(points - 1):
+        j = i + 1
+
+        delta = x_centers[j] - x_centers[i]
+        dist_ij = (
+            (x_centers[j] - x_centers[i]) ** 2 + (y_centers[j] - y_centers[i]) ** 2
+        ) ** 0.5
+        distances[i, j] = distances[j, i] = dist_ij
+
+        while (dist_ij <= delta) and (j < points):
+            j += 1
+            delta = x_centers[j] - x_centers[i]
+            dist_ij = (
+                (x_centers[j] - x_centers[i]) ** 2 + (y_centers[j] - y_centers[i]) ** 2
+            ) ** 0.5
+            distances[i, j] = distances[j, i] = dist_ij
+        
+        maximal_radius = int(np.min(distances[i, :]))
+        if maximal_radius == 0:
+            radiuses[i] = 0
+        else:
+            radiuses[i] = maximal_radius
+            while j >= 0:
+                distances[j, i] -= radiuses[i]
+                j -= 1
+
+    return [Circle(x, y, r) for (x, y, r) in zip(x_centers, y_centers, radiuses)]
+
+if __name__ == "__main__":
+    print(generate_circles((100, 100), 10))
--- tests/datasets/test_generate_circles.py
+++ tests/datasets/test_generate_circles.py
@@ -7,19 +7,17 @@ from deep_circle_counter.dataset.generate_circles import generate_circles
 
 class TestCircleGenerator(unittest.TestCase):
     def test_generate_circles(self):
-        radiuses = [3, 5, 7]
-        height, width = 40, 60
+        size = (40, 60)
+        points = 3
 
-        image = np.zeros((height, width))
+        image = np.zeros((size[0], size[1]))
 
-        circles = generate_circles(height, width, radiuses)
-        yy, xx = np.meshgrid(range(height), range(width))
+        circles = generate_circles(size, points)
+        yy, xx = np.meshgrid(range(size[0]), range(size[1]))
 
-        for center_y, center_x, radius in circles:
+        for circle in circles:
             for y, x in zip(yy.flatten(), xx.flatten()):
-                if (x - center_x) ** 2 + (y - center_y) ** 2 <= radius**2:
+                if (x - circle.x) ** 2 + (y - circle.y) ** 2 <= circle.r**2:
                     image[y, x] += 1
 
-        self.assertCountEqual(np.unique(image), [0.0, 1.0])
-        self.assertEqual(np.sum(image), 259.0)
- 
+        self.assertCountEqual(np.unique(image), [0.0, 1.0]) 
